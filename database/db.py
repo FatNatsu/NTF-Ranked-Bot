fe5dcb7af1c691b7eda4536e8192bf16d1d01f4e
FatNatsu <xdtaiyyab420@gmail.com>
2026-09-08 11:57:02 +0100

    Add database

---
+++ database/db.py
@@ -0,0 +1,43 @@
+import aiosqlite
+
+DB_NAME = "ntf.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS captains(
+            user_id INTEGER PRIMARY KEY
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS teams(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT UNIQUE
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS players(
+            user_id INTEGER PRIMARY KEY,
+            mmr INTEGER DEFAULT 1000,
+            role TEXT DEFAULT 'ANY'
+        )
+        """)
+
+        default_teams = [
+            "Fram Esports",
+            "The Fifth Pass",
+            "Warya Wonders",
+            "Delectable XI",
+            "Joyboi"
+        ]
+
+        for team in default_teams:
+            await db.execute(
+                "INSERT OR IGNORE INTO teams(name) VALUES(?)",
+                (team,)
+            )
+
+        await db.commit()
